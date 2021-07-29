b1e89e3352487957f88a4ca984ae8c2ae149c76f
etnoMorgen <augustofnegromonte@gmail.com>
2021-07-28 22:32:36 -0300

    Adicionar produto ao carrinho e mensagem de sucesso

--- src/App.vue
+++ src/App.vue
@@ -91,6 +91,26 @@ export default {
   data() {
     return {
       showMobileMenu: false,
+      cart: {
+        items: []
+      }
+    }
+  },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+  },
+  mounted() {
+    this.cart = this.$store.state.cart
+  },
+  computed: {
+    cartTotalLength() {
+      let totalLength = 0
+
+      for (let i=0; i<this.cart.items.length; i++){
+        totalLength += this.cart.items[i].quantity
+      }
+
+      return totalLength
     }
   }
 }
